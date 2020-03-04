ae61153560d0d64ce22adab0aee7acfdf24a3c33
Pranav Dixit <dprnav@gmail.com>
2020-03-04 15:15:17 +0530

    server

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 var elasticsearch=require('elasticsearch');
 
 const client = new elasticsearch.Client({
-   hosts: [ 'https://elastic:FM6HSansztuP7mXNGlnnE4FN@f03b7e92942440eeabecfbb4622e9881.us-east-1.aws.found.io:9243'],
+   hosts: [ 'http://13.233.167.54:9200/'],
    requestTimeout: 2147483647
 });
 
--- server.js
+++ server.js
@@ -1,72 +1,119 @@
-var express=require('express');
-var app=express();
+var express = require('express');
+var app = express();
 var client = require('./connection.js');
 var url = require('url');
-var sw = require('stopword');
-var stemmer = require('stemmer');
-var checkWord = require('check-word');
-var words = checkWord('en');
 var cors = require('cors');
 
 app.use(cors());
 app.options('*', cors());
 
-app.set('views',__dirname + '/views');
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-function remove_stopwords_and_stemmer(str) {
-
-    var oldString = str.split(' ');
-    var newString = sw.removeStopwords(oldString);
-    for (var word in newString){
-      newString[word] = stemmer(newString[word]);
-      str = newString[word];
-      var n = str.length;
-      var i = 0;
-      for (var len = 3; len <= n - 3; len++){
-          if(words.check(str.substring(i,len)) && words.check(str.substring(len,n))){
-            newString[word] = str.substring(i,len)+'   '+str.substring(len,n);
-          }
-      }
-    }
-    return(newString.join(' '));
-}
 
 app.get('/search', (req, res) => {
 
   var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
-
   client.search({
     index: 'movie',
-    type: 'movies',
     body: {
       from: 0,
-      size : 10,
+      size: 10,
       query: {
-        match: {
-          MovieName: {
-            query: remove_stopwords_and_stemmer(query.query),
-            fuzziness: "AUTO"
-          }
+        bool: {
+          should: [{
+              match: {
+                "MovieName.exact": query.query
+              }
+            },
+            {
+              match_phrase_prefix: {
+                MovieName: {
+                  query: query.query
+                }
+              }
+            },
+            {
+              match: {
+                MovieName: {
+                  query: query.query,
+                  fuzziness: "AUTO",
+                  prefix_length: 1
+                }
+              }
+            }
+          ]
         }
       }
     }
-  },function (error, response,status) {
-      var result = '[';
-      if (error){
-      }
-      else {
-        response.hits.hits.forEach(function(hit){
-          if (result != '['){
-            result += ',';
-          }
-          result += JSON.stringify(hit._source);
-        });
-        result += ']';
-        res.send(result);
-      }
+  }, function(error, response, status) {
+    var result = '[';
+    if (error) {
+      console.log(error)
+    } else {
+      response.hits.hits.forEach(function(hit) {
+        if (result != '[') {
+          result += ',';
+        }
+        result += JSON.stringify(hit._source);
+      });
+      result += ']';
+      res.send(JSON.parse(result));
+    }
   });
 });
 
-var server=app.listen(process.env.PORT);
+app.get('/providers', (req, res) => {
+
+  var url_parts = url.parse(req.url, true);
+  var imdb = url_parts.query.imdb;
+  var country = url_parts.query.country;
+  var mtype = url_parts.query.mtype;
+  var ptype = url_parts.query.ptype;
+  client.search({
+    index: 'monetization',
+    body: {
+      from: 0,
+      size: 1000,
+      query: {
+        "bool": {
+          "must": [{
+              "match": {
+                "country": country
+              }
+            },
+            {
+              "match": {
+                "imdb": imdb
+              }
+            },
+            {
+              "match": {
+                "mtype": mtype
+              }
+            },
+            {
+              "match": {
+                "ptype": ptype
+              }
+            }
+          ]
+        }
+      }
+    }
+  }, function(error, response, status) {
+    var result = '[';
+    if (error) {
+      console.log(error)
+    } else {
+      response.hits.hits.forEach(function(hit) {
+        if (result != '[') {
+          result += ',';
+        }
+        result += JSON.stringify(hit._source);
+      });
+      result += ']';
+      res.send(JSON.parse(result));
+    }
+  });
+});
+var server = app.listen(5000 || process.env.PORT);
+console.log('Server Started!')
